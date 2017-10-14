b5123fbefdd94571e0db81210f36aed5918f8a65
chenzh79 <chenzh79@126.com>
2017-10-14 15:54:24 -0400

    fix

--- 1/1_E3.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-import System.Environment
- 
-main :: IO ()
-main = do putStrLn "What do they call thee at home?"
-          name <- getLine
-          putStrLn ("Ey up " ++ name)
-
-
